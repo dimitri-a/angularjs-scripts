8c6b4e2090bc218f0be69ad10c37f2824b762cc4
Nargonath <jonas.pauthier@gmail.com>
2017-10-04 14:23:30 +0200

    feat(eslint): Add @dixeed/eslint-config. Export eslint config
    Add @dixeed/eslint-config/angularjs to eslint webpack build part

--- config/eslint/eslint.config.js
+++ config/eslint/eslint.config.js
@@ -1,22 +1,5 @@
 'use strict';
 
 module.exports = {
-  extends: ['eslint:recommended', 'angular'],
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-    },
-  },
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  rules: {
-    'angular/no-service-method': 'off',
-    'angular/log': 'warn',
-    'no-console': 'warn',
-  },
+  extends: ['@dixeed/eslint-config/angularjs'],
 };
